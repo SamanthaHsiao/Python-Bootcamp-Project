c55c5342d52f1c981cb2589c0baec3386cac2fc6
Hsiao Jou Samantha Yu <samantha19980119@gmail.com>
2025-05-20 18:09:31 +0800

    Update Reeborg's World.py

--- Reeborg's World.py
+++ Reeborg's World.py
@@ -23,3 +23,21 @@ while not at_goal():
         jump()
     if front_is_clear():
         move()
+--Maze--
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
